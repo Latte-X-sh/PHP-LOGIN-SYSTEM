59291e99f47db4db498de1df6b9b0aa41d4f5d29
Franklin Shera <fshera96@gmail.com>
2021-08-22 18:50:54 +0300

    Mix Upload Refactor

--- lib/functions.php
+++ lib/functions.php
@@ -1,16 +1,19 @@
 <?php
 //function that connects my web page and the database
-function get_connection(){
+function get_connection()
+{
     $config = require 'config.php';
     //WE NEED TO HAVE A FAILOVER INCASE THE DB CONNECTION AND SO WE NEED TO HAVE AN ERROR MECHANISM MODULE
-    try{
-        return new PDO($config['database_dsn'], $config['database_user'] , $config['database_password']);  
-    } catch(PDOException $e){
+    try {
+        return new PDO($config['database_dsn'], $config['database_user'], $config['database_password']);
+    } catch (PDOException $e) {
         die("The connection to the database is faulty " . $e->getMessage());
     }
-    
+
 }
-function fetch_db(){ //works well(fetches data from the db)
+
+function fetch_db()
+{ //works well(fetches data from the db)
     $pdo = get_connection();
     // var_dump($pdo); die();
     $query = "SELECT * FROM users";
@@ -19,69 +22,79 @@ function fetch_db(){ //works well(fetches data from the db)
     return $users;
 
 }
-function fetch_user_data($userdata){
+
+function fetch_user_data($userdata)
+{
     //get connections
     $pdo = get_connection();
     //prepare the query to the database.SQL statement
-    $query ="SELECT * from users where id='$userdata' ";
+    $query = "SELECT * from users where id='$userdata' ";
     $results = $pdo->query($query);
-    $user= $results->fetch();
+    $user = $results->fetch();
     return $user;
 }
-function save_to_db($nme,$emil,$pwd){
-    // var_dump($nme,$emil,$pwd);die();
-    try{
-    $pdo = get_connection();
-    $query = "INSERT into `users` ( `name`, `email`, `password`) VALUES (:name, :email, :password)";
-    $stmt = $pdo->prepare($query);
-    $stmt->bindParam(":name", $nme);
-    $stmt->bindParam(":email", $emil);
-    $stmt->bindParam(":password", $pwd);
-    $result = $stmt->execute(array(
-        ':name'=> $nme,
-        ':email'=> $emil,
-        ':password'=>$pwd,
-     
-    ));
-    // var_dump($stmt);die();
-}
-catch(PDOException $e){
 
+function save_to_db($nme, $emil, $pwd)
+{
+    // var_dump($nme,$emil,$pwd);die();
+    try {
+        $pdo = get_connection();
+        $query = "INSERT into `users` ( `name`, `email`, `password`) VALUES (:name, :email, :password)";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":name", $nme);
+        $stmt->bindParam(":email", $emil);
+        $stmt->bindParam(":password", $pwd);
+        $result = $stmt->execute(array(
+            ':name' => $nme,
+            ':email' => $emil,
+            ':password' => $pwd,
+
+        ));
+        // var_dump($stmt);die();
+    } catch (PDOException $e) {
+
+    }
 }
-}
-function get_old_password($user_email){
+
+
+function get_old_password($user_email)
+{
     $pdo = get_connection();
-    $query="SELECT `password` FROM `users` WHERE email= :email";
+    $query = "SELECT `password` FROM `users` WHERE email= :email";
     $stmt = $pdo->prepare($query);
-    $result= $stmt->execute(array(
-        ':email'=> $user_email
+    $result = $stmt->execute(array(
+        ':email' => $user_email
     ));
-    $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+    $userRow = $stmt->fetch(PDO::FETCH_ASSOC);
     $oldpassword = $userRow['password'];
     return $oldpassword;
     // echo $oldpassword ;die();
 }
-function update_password($newpass,$email,$passold){
-$pdo = get_connection();
-$oldpass = get_old_password($email);
-if($passold === $oldpass){
-$query="UPDATE `users` SET password=:newpass where email= :email";
-$stmt = $pdo->prepare($query);
-$stmt->bindParam(":newpass", $newpass);
-$stmt->bindParam(":email", $email);
-$result=$stmt->execute(array(
-    ':newpass'=> $newpass,
-    ':email'=> $email,
-));
-    header("Location:Account-settings.php?success=Password Successfylly updated");
-}else{
-    header("Location:Account-settings.php?error=Wrong Old Password");
+
+function update_password($newpass, $email, $passold)
+{
+    $pdo = get_connection();
+    $oldpass = get_old_password($email);
+    if ($passold === $oldpass) {
+        $query = "UPDATE `users` SET password=:newpass where email= :email";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":newpass", $newpass);
+        $stmt->bindParam(":email", $email);
+        $result = $stmt->execute(array(
+            ':newpass' => $newpass,
+            ':email' => $email,
+        ));
+        header("Location:Account-settings.php?success=Password Successfylly updated");
+    } else {
+        header("Location:Account-settings.php?error=Wrong Old Password");
+    }
 }
-}
-function update_db_data($array_from_side,$iduser){
-    $pdo=get_connection();
+
+function update_db_data($array_from_side, $iduser)
+{
+    $pdo = get_connection();
 //Query for name
-$query1="UPDATE `users` 
+    $query1 = "UPDATE `users` 
                 SET name = CASE  
                             WHEN :name is NULL THEN users.name
                             WHEN :name='' THEN users.name 
@@ -89,63 +102,63 @@ $query1="UPDATE `users`
                             END  
                 WHERE id = :id ";
 //Query for firstname
-$query2="UPDATE `users`  
+    $query2 = "UPDATE `users`  
                 SET First_name = CASE 
                             WHEN :fname is NULL THEN users.First_name
                             WHEN :fname='' THEN users.First_name 
                             ELSE :fname 
                             END  
-                WHERE id = :id ";   
+                WHERE id = :id ";
 //Query for lastname
-$query3="UPDATE `users`  
+    $query3 = "UPDATE `users`  
                 SET Last_name = CASE  
                             WHEN :lname  is NULL THEN users.Last_name 
                             WHEN :lname='' THEN users.Last_name 
                             ELSE :lname 
                             END  
-                WHERE id = :id ";  
+                WHERE id = :id ";
 //Query for Birthdate
-$query4="UPDATE `users`  
+    $query4 = "UPDATE `users`  
                 SET date_of_birth = CASE 
                             WHEN :dbirth  is NULL THEN  users.date_of_birth
                             WHEN :dbirth='' THEN users.date_of_birth  
                             ELSE :dbirth 
                             END  
-                WHERE id = :id ";  
+                WHERE id = :id ";
 //Query for Email
-$query5="UPDATE `users`  SET email = CASE 
+    $query5 = "UPDATE `users`  SET email = CASE 
                             WHEN :email is NULL THEN  users.email
                             WHEN :email='' THEN users.email 
                             ELSE :email 
                             END  
-                WHERE id = :id ";  
+                WHERE id = :id ";
 //Query for Country
-$query6="UPDATE `users`  SET Country = CASE 
+    $query6 = "UPDATE `users`  SET Country = CASE 
                          WHEN :country is NULL THEN users.Country
                          WHEN :country='' THEN users.Country 
                          ELSE :country
                          END  
-                WHERE id = :id "; 
+                WHERE id = :id ";
 //Query for City
-$query7="UPDATE `users`  SET City = CASE 
+    $query7 = "UPDATE `users`  SET City = CASE 
                          WHEN :city is NULL THEN  users.City 
                          WHEN :city='' THEN users.City 
                          ELSE :city 
                          END 
-                WHERE id = :id ";  
+                WHERE id = :id ";
 //Query for Phone number
-$query8="UPDATE `users`  SET Phone_number = CASE 
+    $query8 = "UPDATE `users`  SET Phone_number = CASE 
                          WHEN :phoneno  is NULL THEN users.Phone_number
                          WHEN :phoneno='' THEN users.Phone_number 
                          ELSE :phoneno 
                          END 
-                WHERE id = :id ";  
+                WHERE id = :id ";
 //Query for profile picture and gender pending 
-  $testarray=array(
-    $query1,$query2,$query3,$query4,$query5,$query6,$query7,$query8
-  );
-  $size= sizeof($testarray);
-  $limit =$size-1;//7
+    $testarray = array(
+        $query1, $query2, $query3, $query4, $query5, $query6, $query7, $query8
+    );
+    $size = sizeof($testarray);
+    $limit = $size - 1;//7
 //   echo $testarray[0];  die();
 
 
@@ -174,100 +187,101 @@ $query8="UPDATE `users`  SET Phone_number = CASE
     //         ':fname'=> $array_from_side['firstname'],
     //         ':lname'=> $array_from_side['lastname'],
     //         ':id'=> $iduser
-            
+
     //     ));
     //     unset($query);
 
 
-    for($i=0;$i<=$limit-5;$i++){
+    for ($i = 0; $i <= $limit - 5; $i++) {
         //executes this queries until successful
-        $query=$testarray[$i];
-        $stmt=$pdo->prepare($query);
-        $stmt->bindParam(':name',$array_from_side['username']);
-        $stmt->bindParam(':fname',$array_from_side['firstname']);
-        $stmt->bindParam(':lname',$array_from_side['lastname']);
-        $stmt->bindParam(':id',$iduser);
-        $result=$stmt->execute(array(
-            ':name'=> $array_from_side['username'],
-            ':fname'=> $array_from_side['firstname'],
-            ':lname'=> $array_from_side['lastname'],
-            ':id'=> $iduser
-            
+        $query = $testarray[$i];
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':name', $array_from_side['username']);
+        $stmt->bindParam(':fname', $array_from_side['firstname']);
+        $stmt->bindParam(':lname', $array_from_side['lastname']);
+        $stmt->bindParam(':id', $iduser);
+        $result = $stmt->execute(array(
+            ':name' => $array_from_side['username'],
+            ':fname' => $array_from_side['firstname'],
+            ':lname' => $array_from_side['lastname'],
+            ':id' => $iduser
+
         ));
         unset($query);
-        }
-    for($y=3;$y<=$limit-2;$y++){
-            //executes this queries until successful
-            $query=$testarray[$y];
-            $stmt=$pdo->prepare($query);
-            $stmt->bindParam(':dbirth',$array_from_side['birthdate']);
-            $stmt->bindParam(':email',$array_from_side['email']);
-            $stmt->bindParam(':country',$array_from_side['country']);
-            $stmt->bindParam(':id',$iduser);
-            $result=$stmt->execute(array(
-                 ':dbirth'=> $array_from_side['birthdate'],
-                ':email'=> $array_from_side['email'],
-                ':country'=> $array_from_side['country'],
-                ':id'=> $iduser
-                
-            ));
-            unset($query);
-            }
-            for($y=6;$y<=$limit;$y++){
-                //executes this queries until successful
-                $query=$testarray[$y];
-                $stmt=$pdo->prepare($query);
-                $stmt->bindParam(':city',$array_from_side['city']);
-                $stmt->bindParam(':phoneno',$array_from_side['phoneno']);
-                $stmt->bindParam(':id',$iduser);
-                $result=$stmt->execute(array(
-                    ':city'=> $array_from_side['city'],
-                    ':phoneno'=> $array_from_side['phoneno'],
-                    ':id'=> $iduser
-                    
-                ));
-                unset($query);
-               }
-        //  echo "status code 1- moment of truth "; die();
+    }
+    for ($y = 3; $y <= $limit - 2; $y++) {
+        //executes this queries until successful
+        $query = $testarray[$y];
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':dbirth', $array_from_side['birthdate']);
+        $stmt->bindParam(':email', $array_from_side['email']);
+        $stmt->bindParam(':country', $array_from_side['country']);
+        $stmt->bindParam(':id', $iduser);
+        $result = $stmt->execute(array(
+            ':dbirth' => $array_from_side['birthdate'],
+            ':email' => $array_from_side['email'],
+            ':country' => $array_from_side['country'],
+            ':id' => $iduser
+
+        ));
+        unset($query);
+    }
+    for ($y = 6; $y <= $limit; $y++) {
+        //executes this queries until successful
+        $query = $testarray[$y];
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':city', $array_from_side['city']);
+        $stmt->bindParam(':phoneno', $array_from_side['phoneno']);
+        $stmt->bindParam(':id', $iduser);
+        $result = $stmt->execute(array(
+            ':city' => $array_from_side['city'],
+            ':phoneno' => $array_from_side['phoneno'],
+            ':id' => $iduser
+
+        ));
+        unset($query);
+    }
+    //  echo "status code 1- moment of truth "; die();
     //echo status code 1 and js takes it and alerts us that we are successfull
-    
+
     // var_dump($stmt);die();
 }
-function login_db($emil,$pwd){
+
+function login_db($emil, $pwd)
+{
     $pdo = get_connection();
-    $query ="SELECT * FROM `users` WHERE email= :email AND password= :pass";
+    $query = "SELECT * FROM `users` WHERE email= :email AND password= :pass";
     $stmt = $pdo->prepare($query);
     $result = $stmt->execute(array(
-        ':email'=> $emil,
-        ':pass'=>$pwd,
+        ':email' => $emil,
+        ':pass' => $pwd,
     ));
     // $row = $stmt->fetch() //the 1st implemention inside the if statement where $userRow is @
-    $userRow=$stmt->fetch(PDO::FETCH_ASSOC); //returns an associative array that contains the data from the db
+    $userRow = $stmt->fetch(PDO::FETCH_ASSOC); //returns an associative array that contains the data from the db
 
-    if($stmt->rowCount() == 1){
-        if($userRow){
-        $id = $userRow["id"];
-        $email = $userRow["email"];
-        $name = $userRow["name"];
-        $password = $userRow["password"];
-        // $truth = password_verify($pwd,$password); //this code is useful only when i've hashed my passwords.
-        //  echo $id," ",$email," ",$name ;//this code echo the value from the database if they correct .
-            if($pwd == $password){
+    if ($stmt->rowCount() == 1) {
+        if ($userRow) {
+            $id = $userRow["id"];
+            $email = $userRow["email"];
+            $name = $userRow["name"];
+            $password = $userRow["password"];
+            // $truth = password_verify($pwd,$password); //this code is useful only when i've hashed my passwords.
+            //  echo $id," ",$email," ",$name ;//this code echo the value from the database if they correct .
+            if ($pwd == $password) {
                 session_start();
                 $_SESSION["loggedin"] = true;
                 $_SESSION["id"] = $id; //random number
                 $_SESSION["name"] = $name;
                 $_SESSION["email"] = $email;
                 header('location:/login module/index.php');
-                                                }
-                else{
-                        $login_err="Invalid email or password.";
-                    }
-            }else{
-                $login_err="Invalid email or password.";
+            } else {
+                $login_err = "Invalid email or password.";
             }
-    }else{
-        $login_err="Invalid email or password";//if the statement return a number not 1(true) then display this error message.
+        } else {
+            $login_err = "Invalid email or password.";
+        }
+    } else {
+        $login_err = "Invalid email or password";//if the statement return a number not 1(true) then display this error message.
     }
     // Close statement
     unset($stmt);
@@ -277,4 +291,21 @@ function login_db($emil,$pwd){
 // $email="mitchwangui@gmail.com";
 // $password="123456";
 //  login_db($email , $password);
- 
+
+
+
+
+
+//BY SHERA
+function sanitizeText($textString) {
+    return stripcslashes(trim(htmlspecialchars($textString)));
+}
+
+
+
+
+function handle_audio($mixTitle,$mixFile){
+
+
+
+}
---
+++ mix_upload.php
@@ -0,0 +1,136 @@
+﻿<?php
+// Initialize the session
+session_start();
+require 'lib/functions.php';
+require 'lib/imageprocessing.php';
+$user_id = $_SESSION['id'];
+$user_email = $_SESSION['email'];
+// var_dump($_SESSION);die();
+// echo $user_email ; die();
+$profile_data = fetch_user_data($user_id);//Fetches db data
+// var_dump($_SESSION);die();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $mixTitle = null;
+    $mixFile = null;
+
+
+
+    if(isset($_POST['mix-title'])){
+        $mixTitle = $_POST['mix-title'];
+    }
+
+
+    if(isset($_POST['mix-file'])){
+        $mixFile = $_POST['mix-file'];
+    }
+
+    handle_audio($mixTitle,$mixFile);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="Smarthr - Bootstrap Admin Template">
+    <meta name="keywords"
+          content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern, accounts, invoice, html5, responsive, CRM, Projects">
+    <meta name="author" content="Dreamguys - Bootstrap Admin Template">
+    <meta name="robots" content="noindex, nofollow">
+    <title>DJ Mmix Upload</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="assets/css/line-awesome.min.css">
+
+    <link rel="stylesheet" href="assets/css/select2.min.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap-datetimepicker.min.css">
+
+    <link rel="stylesheet" href="assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.min.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- The navbar section is modular and will be called when needed. -->
+<?php require 'layout/header.php'; ?>
+<!-- sidebar section 310 to 597 .Inside the layout folder and we will require it -->
+<?php require 'layout/sidemenu.php'; ?>
+<!-- main page -->
+<div class="page-wrapper">
+
+    <div class="content container-fluid">
+
+        <div class="page-header">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3 class="page-title">Profile</h3>
+                    <!-- Breadcrumb on the page -->
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Mix Upload</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+        <form method="post" action="mix_upload.php" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-md-12 col-sm-6">
+                    <div class="form-group">
+                        <label>Mix Title</label>
+                        <input class="form-control" name="mix-title" type="text">
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="form-group">
+                <label>Upload Files</label>
+                <input class="form-control" name="mix-file" accept="audio/*" type="file">
+            </div>
+            <div class="submit-section">
+                <button class="btn btn-primary submit-btn">Upload</button>
+            </div>
+        </form>
+
+
+    </div>
+
+
+</div>
+
+</div>
+
+
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+
+<script src="assets/js/select2.min.js"></script>
+
+<script src="assets/js/moment.min.js"></script>
+<script src="assets/js/bootstrap-datetimepicker.min.js"></script>
+
+<script src="assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.min.js"></script>
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
